e9847b05874e947043fc3bce5f4e909cfcac96a2
Carlos-Eduardo-Guedes-01 <carloseduardoguedes89981456761@gmail.com>
2024-07-07 11:45:16 -0300

    Tipos e operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        // Operador ternário
+        String resultado = a == b ? "verdadeiro" : "falso";
+        /*
+         * String resultado = "";
+         * 
+         * if (a == b) {
+         * resultado = "verdadeiro";
+         * } else {
+         * resultado = "falso";
+         * }
+         */
+        System.out.println("a==b é " + resultado);
+    }
+}
